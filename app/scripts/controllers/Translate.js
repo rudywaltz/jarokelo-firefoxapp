7465e0f1779a1bc5a627bf6ac20a5e59c51211cd
David Barkoczi <david.barkoczi@gmail.com>
2013-11-18 00:38:36 +0100

    init listview

--- app/scripts/controllers/Translate.js
+++ app/scripts/controllers/Translate.js
@@ -4,7 +4,6 @@ angular.module('jarokeloApp')
   .controller('TranslateCtrl', ['$translate', '$scope',
   function ($translate, $scope) {
   $scope.changeLanguage = function (langKey) {
-    console.log('change');
     $translate.uses(langKey);
   };
 }]);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,12 +11,15 @@ jarokelo.config(function ($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: 'views/main.html',
       // controller: 'MainCtrl'
+    })
+    .state('city', {
+      url: '/city',
+      templateUrl: 'views/city.html',
+    })
+    .state('list', {
+      url: '/list',
+      templateUrl: 'views/list.html',
     });
-    // .state('user', {
-    //   url: '/user',
-    //   templateUrl: 'iews/user.html',
-    //   // controller: 'UserCtrl'
-    // });
 });
 
 jarokelo.config(['$translateProvider', function ($translateProvider) {
@@ -24,13 +27,25 @@ jarokelo.config(['$translateProvider', function ($translateProvider) {
       HEADLINE: 'Jarokelo.hu',
       LANG_SELECT_HU:    'hungarian',
       LANG_SELECT_EN:    'english',
-      CHOOSE_ONE_OPTION: 'Choose one option'
+      CHOOSE_ONE_OPTION: 'Choose one option',
+      CITY: 'City',
+      REPORT: 'Report',
+      LIST_VIEW: 'List view'
     })
     .translations('hu', {
       HEADLINE: 'Járókelő.hu',
       LANG_SELECT_HU: 'magyar',
       LANG_SELECT_EN: 'angol',
-      CHOOSE_ONE_OPTION: 'Válasszon egy lehetőséget'
+      CHOOSE_ONE_OPTION: 'Válasszon egy lehetőséget',
+      CITY: 'Város',
+      REPORT: 'Bejelentés',
+      LIST_VIEW: 'lista nézet'
     });
     $translateProvider.preferredLanguage('hu');
   }]);
+
+var server = window.location.protocol + '//' + window.location.hostname;
+if (window.location.port && window.location.port !== '9000') {
+  server = server + ':' + window.location.port;
+}
+angular.module('configuration', []).constant('API_SERVER_ENDPOINT', server);
