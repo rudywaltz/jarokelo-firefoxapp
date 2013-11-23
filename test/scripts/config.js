e1b50705130601c80794d78d9b3a7e901fe8e977
David Barkoczi <david.barkoczi@gmail.com>
2013-11-23 16:38:28 +0100

    merge

---
+++ test/scripts/config.js
@@ -0,0 +1,4 @@
+'use strict';
+//live-site:  http://www.odkazprestarostu.sk'
+angular.module('configuration', []).constant('API_SERVER_ENDPOINT',
+ 'http://www.sandbox.odkazprestarostu.sk');
---
+++ test/scripts/controllers/list.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('jarokeloApp')
+  .controller('ListCtrl', ['$scope', '$http', 'API_SERVER_ENDPOINT',
+		function ($scope, $http, $server) {
+			$scope.server = $server;
+      $http.get($server + '/api/list')
+      .success(function (data) {
+        $scope.list = data.podnety;
+      });
+    }]);
